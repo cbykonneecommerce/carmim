8532898d4d2b12abf49e6be9ddc23a5a9457cb87
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-02-24 19:45:54 -0300

    aa

---
+++ js/menu.js
@@ -0,0 +1,159 @@
+
+$(".depto-FEMININO").hover(()=>{
+    
+    $(".depto-FEMININO#FEMININO").show()
+}, 
+()=>{
+    $(".depto-FEMININO#FEMININO").hide()
+})
+
+
+$(".depto-MASCULINO").hover(()=>{
+    
+    $(".depto-MASCULINO#MASCULINO").show()
+}, 
+()=>{
+    $(".depto-MASCULINO#MASCULINO").hide()
+})
+
+$(".depto-BOLSAS").hover(()=>{
+    
+    $(".depto-BOLSAS#BOLSAS").show()
+}, 
+()=>{
+    $(".depto-BOLSAS#BOLSAS").hide()
+})
+
+$(".depto-ACESSÓRIOS").hover(()=>{
+    
+    $(".depto-ACESSÓRIOS#ACESSÓRIOS").show()
+}, 
+()=>{
+    $(".depto-ACESSÓRIOS#ACESSÓRIOS").hide()
+})
+
+$(".depto-CALÇADOS").hover(()=>{
+    
+    $(".depto-CALÇADOS#CALÇADOS").show()
+}, 
+()=>{
+    $(".depto-CALÇADOS#CALÇADOS").hide()
+})
+$(".depto-SALE").hover(()=>{
+    
+    $(".depto-SALE#SALE").show()
+}, 
+()=>{
+    $(".depto-SALE#SALE").hide()
+})
+
+
+
+
+//  Controle para menu mobile 
+
+/*
+$(".dropdown-btn#rouparia").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#rouparia i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#rouparia").slideDown()
+}, () => {
+    $(".dropdown-container#rouparia").hide()
+    $(".dropdown-btn#rouparia i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#tableware").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#tableware i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#tableware").slideDown()
+}, () => {
+    $(".dropdown-container#tableware").hide()
+    $(".dropdown-btn#tableware i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#decor").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#decor i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#decor").slideDown()
+}, () => {
+    $(".dropdown-container#decor").hide()
+    $(".dropdown-btn#decor i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#moveis").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#moveis").slideDown()
+}, () => {
+    $(".dropdown-container#moveis").hide()
+    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#presentes").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#presentes i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#presentes").slideDown()
+}, () => {
+    $(".dropdown-container#presentes").hide()
+    $(".dropdown-btn#presentes i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#aromas").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#aromas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#aromas").slideDown()
+}, () => {
+    $(".dropdown-container#aromas").hide()
+    $(".dropdown-btn#aromas i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#baby").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#baby i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#baby").slideDown()
+}, () => {
+    $(".dropdown-container#baby").hide()
+    $(".dropdown-btn#baby i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#custom").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#moveis i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#custom").slideDown()
+}, () => {
+    $(".dropdown-container#custom").hide()
+    $(".dropdown-btn#custom i").attr('class', 'fa fa-angle-down');
+})
+
+
+$(".dropdown-btn#marcas").toggle(() => {
+    $(".dropdown-container").hide();
+    $(".dropdown-btn#marcas i").attr('class', 'fa fa-angle-up');
+    $(".dropdown-container#marcas").slideDown()
+}, () => {
+    $(".dropdown-container#marcas").hide()
+    $(".dropdown-btn#marcas i").attr('class', 'fa fa-angle-down');
+})
+
+
+
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()> 150)
+     {
+        $('#storenavsecondary').fadeOut();
+     }
+    else
+     {
+      $('#storenavsecondary').fadeIn();
+     }
+ });
+*/
