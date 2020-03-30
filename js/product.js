ec230ecd35d5eb1097e905d7e92d8f292cb50c73
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-03-30 16:59:27 -0300

    a

--- js/product.js
+++ js/product.js
@@ -7,7 +7,7 @@ setTimeout(function () {
     $(".glis-popup-link.glis-thickbox.tb-added.qvBinded", "body").html(`<img src="/arquivos/CORACAO2.png" style="max-width: 18px"> ADICIONAR A LISTA DE DESEJOS`);
     $("#TB_closeWindowButton").html(`<img src="/arquivos/menu-close.png" style="max-width:25px"/>`);
     $(".user-review h4").text("AVALIAÇÕES");
-}, 1000);
+}, 2000);
 
 
 $(".dropdown-container").hide();
--- js/vendedor.js
+++ js/vendedor.js
@@ -1,11 +1,11 @@
 
 
-setTimeout(()=>{
-    $(".mini-cart-footer").prepend(`
+setTimeout(() => {
+  $(".mini-cart-footer").prepend(`
     <div class="lojista-codigo" style="margin-bottom: 20px;">
-    <label style="margin-right: 2em;">Código do vendedor</label>
+    <label>Se for um vendedor de nossas lojas<br> inclua seu código aqui</label>
     <input type="text" id="codigologista" placeholder="Vendedor, insira seu código aqui" style="border-radius: 0px;
-    width: 52%;
+    width: 66%;
     border-color: black;
     padding-left: 8px"/>
     </div>
@@ -13,32 +13,53 @@ setTimeout(()=>{
 
 
 
-    $(".lojista-codigo #codigologista").on("change paste keyup",  async function(){
+  $(".lojista-codigo #codigologista").on("change paste keyup", async function () {
     let vendedorCode = $("#codigologista").val();
+    let codeConfirmation = false;
     console.log(vendedorCode)
     let results = await fetch("/api/dataentities/CV/search?_fields=codigo")
-                            .then(response =>{
-                              return response.json();  
-                            });
-    if(!results)
+      .then(response => {
+        return response.json();
+      });
+    console.log(results);
+
+      for(let i = 0; i < results.length; i++) {
+        if(vendedorCode == results[i].codigo) {
+          codeConfirmation = true;
+          break;
+        }
+      }
+
+      if(codeConfirmation) {
+        $("#codigologista").css({"border-color": "black"});
+        $(".lojista-codigo small").hide()
+
+        vtexjs.checkout.getOrderForm()
+        .then(function (orderForm) {
+          let marketingData = orderForm.marketingData;
+          marketingData.utmSource = vendedorCode;
+          return vtexjs.checkout.sendAttachment('marketingData', marketingData)
+        }).done(function (orderForm) {
+          console.log("Nome alterado!");
+          console.log(orderForm.marketingData);
+        })
+      } else {
+        $("#codigologista").css({"border-color": "red"});
+        $(".lojista-codigo small").remove()
+        $(".lojista-codigo").append("<small><br/>Digite um código de vendedor valido</small>")
+      }
+
     
 
 
-    vtexjs.checkout.getOrderForm()
-    .then(function(orderForm) {
-        let marketingData =  orderForm.marketingData;
-     marketingData.utmSource = vendedorCode;
-      return vtexjs.checkout.sendAttachment('marketingData', marketingData)
-    }).done(function(orderForm) {
-      console.log("Nome alterado!");
-      console.log(orderForm.marketingData);
-    })
 
-})
    
 
+  })
 
 
 
- 
-},1000)
+
+
+
+}, 1000)
